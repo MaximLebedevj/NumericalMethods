d12b4ccf3151d39d956ef77d0d701eb9be018dc8
maximlebedevj <maximlebedevj@gmail.com>
2024-05-26 20:46:53 +1000

    pure c++ version

---
+++ lab4/c++/bin/CMakeFiles/lab4.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maxleb/Git/NumericalMethods/lab4/c++/lab4_ram_matplot++.cpp" "CMakeFiles/lab4.dir/lab4_ram_matplot++.o" "gcc" "CMakeFiles/lab4.dir/lab4_ram_matplot++.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab4/c++/bin/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o.d
@@ -0,0 +1,4 @@
+matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o: \
+ /home/maxleb/Git/NumericalMethods/lab4/c++/matplotplusplus/source/matplot/util/popen.cpp \
+ /usr/include/stdc-predef.h \
+ /home/maxleb/Git/NumericalMethods/lab4/c++/matplotplusplus/source/matplot/util/popen.h
