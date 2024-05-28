5cba42e42a16506b6dfb891a182948a266c4cc88
maximlebedevj <maximlebedevj@gmail.com>
2024-05-28 21:06:24 +1000

    lab7 result

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -4,7 +4,7 @@ import sympy as sp
 
 
 def P(x_):
-    return 1/2 / (x_ + 1)
+    return 1 / 2 / (x_ + 1)
 
 
 def Q(x_):
@@ -13,18 +13,6 @@ def Q(x_):
 
 def F(x_):
     return - 1 * sp.sqrt(x_ + 1)
-
-
-def p(x_):
-    return sp.sqrt(x_ + 1)
-
-
-def q(x):
-    return p(x) * Q(x)
-
-
-def f(x):
-    return p(x) * F(x)
 
 
 def phi0(x):
@@ -43,11 +31,11 @@ def phik_df(k, x):
     return k * x**(k-1) * (1 - x) - x**k
 
 
-def u(ci, x_):
+def numerical(ci, x_):
     sigma = 0.0
     for i in range(len(ci)):
-        sigma += ci[i] * (phik.subs({k: i + 1, x: x_}).evalf())
-    return (phi0.subs(x, x_).evalf() + sigma).evalf()
+        sigma += ci[i] * (phik(k, x).subs({k: i + 1, x: x_}).evalf())
+    return (phi0(x).subs(x, x_).evalf() + sigma).evalf()
 
 
 def exact(x_):
@@ -55,15 +43,16 @@ def exact(x_):
 
 
 def equation(u):
-    return sp.diff(u, x, 2) + P * sp.diff(u, x, 1) + Q * u 
+    return sp.diff(u, x, 2) + P * sp.diff(u, x, 1) + Q * u - F
 
 
-# Количество C
-n = 2
+# Параметры 
+# n - кол-во Ck
+# N - ков-во разбиений
+n = 1
 a = 0
 b = 1
-N = 50
-
+N = 20
 
 x = sp.Symbol('x')
 k = sp.Symbol('k')
@@ -75,8 +64,9 @@ F = F(sp.Symbol('x'))
 Q = Q(sp.Symbol('x'))
 P = P(sp.Symbol('x'))
 
+xi = np.linspace(a, b, N + 1)
+
 print("\nphi0    = ", phi0(x))
-#print(phi0(x).subs(x, 1).evalf())
 print("phi0_df = ", phi0_df(x))
 print("phik    =", phik(k, x))
 print("phik_df =", phik_df(k, x))
@@ -101,83 +91,50 @@ print("\nu(x) = ", ux)
 R = equation(ux)
 print("\nR(x, C1, ..., Cn) = ", R)
 
-
+# СЛАУ 
 systemCi = []
 for i in range(1, n + 1):
-    systemCi.append(sp.simplify(sp.integrate(phik(k, x).subs(k, i) * R, (x, a, b)).evalf()))
-
+    systemCi.append(sp.simplify(sp.integrate(phik(k, x).subs(k, i) * R, (x, a, b))).evalf())
 print("\nsystemCi =", systemCi)
 
+# Решение СЛАУ
+systemCi_sol = list(sp.linsolve(systemCi, [C_k[i] for i in range(0, n)]))[0]
 
-
-#print(phi0.subs(x, 1).evalf())
-
-#xi = np.linspace(a, b, N + 1)
-
-#phi0 = phi0(sp.Symbol('x'))
-#phi0_df = phi0_df(sp.Symbol('x'))
-#phik = phik(sp.Symbol('k'), sp.Symbol('x'))
-#phik_df = phik_df(sp.Symbol('k'), sp.Symbol('x'))
-
-# Задаем количество C_k
-#C_k = [C[j] for j in range(0, n)]
-#print(C_k)
-
-#sigma_str = ""
-#for i in range(0, n):
-#    sigma_str += str(C_k[i] * phik_df.subs(k, i + 1))
-#    if i != n - 1:
-#        sigma_str += " + "
-#sigma1 = eval(sigma_str)
-#print("sigma1 = ", sigma1)
-
-#sigma_str = ""
-#for i in range(0, n):
-#    sigma_str += str(C_k[i] * phik.subs(k, i + 1))
-#    if i != n - 1:
-#        sigma_str += " + "
-#sigma2 = eval(sigma_str)
-#print("sigma2 = ", sigma2)
-
-# Считаем значения dФ / dC_i
-#systemCi = []
-#for i in range(1, n + 1):
-#    systemCi.append(sp.simplify(sp.integrate(2 * p * (phi0_df + sigma1) * phik_df.subs(k, i) -
-#                                             2 * q * (phi0 + sigma2) * phik.subs(k, i) + 2 * f * phik.subs(k, i),
-#                                             (x, a, b))).evalf())
-
-#for i in range(len(systemCi)):
-#    print(systemCi[i])
-
-#system = []
-#for i in range(len(systemCi)):
-#    system.append(systemCi[i])
-#print(system)
-
-# Решение получившейся системы
-#C_linsolve = sp.linsolve(system, [C_k[i] for i in range(0, n)])
-#C = list(C_linsolve)[0]
-#print("C_i values: ", C)
+print()
+for i in range(0, n):
+    print("C{0}".format(i) ," = ", systemCi_sol[i])
 
 # Численное решение
-#u_numeric = []
-#for i in range(N + 1):
-#    u_numeric.append(u(C, xi[i]))
-#print("u_numeric: ", u_numeric)
+u_numerical = []
+for i in range(N + 1):
+    u_numerical.append(numerical(systemCi_sol, xi[i]))
+print("\nu_numerical: ", u_numerical)
 
 # Точное решение
-#u_exact = []
-#for i in range(N + 1):
-    #    u_exact.append(exact(xi[i]))
-#print("u_exact: ", u_exact)
+u_exact = []
+for i in range(N + 1):
+    u_exact.append(exact(xi[i]))
+print("\nu_exact: ", u_exact)
+
+# Невязка
+error = []
+for i in range(N + 1):
+    error.append(abs(u_exact[i] - u_numerical[i]))
+print("\nerror : ", error)
 
 # Визуализация
-#plt.figure(figsize=(15, 7))
-#sp = plt.subplot(121)
+plt.figure(figsize=(15, 7))
+sp = plt.subplot(121)
 
-#plt.plot(xi, u_numeric, 'k', label="Численное")
-#plt.plot(xi, u_exact, 'red', label="Точное")
+plt.plot(xi, u_numerical, 'k', label="Численное")
+plt.plot(xi, u_exact, 'red', label="Точное")
+plt.legend()
+plt.grid(True)
 
-#plt.legend()
-#plt.grid(True)
-#plt.show()
+sp = plt.subplot(122)
+plt.plot(xi, error, 'black')
+plt.plot(xi, error, 'bo', label="Погрешность")
+
+plt.legend()
+plt.grid(True)
+plt.show()
