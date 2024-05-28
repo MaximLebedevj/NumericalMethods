867905d5cedd184e2f85fda7c22efb5a306d536d
MaximLebedevj <maximlebedevj@gmail.com>
2024-05-28 14:48:08 +1000

    added lab7

---
+++ lab7/lab7.py
@@ -0,0 +1,183 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+
+
+def P(x_):
+    return 1/2 / (x_ + 1)
+
+
+def Q(x_):
+    return -1
+
+
+def F(x_):
+    return - 1 * sp.sqrt(x_ + 1)
+
+
+def p(x_):
+    return sp.sqrt(x_ + 1)
+
+
+def q(x):
+    return p(x) * Q(x)
+
+
+def f(x):
+    return p(x) * F(x)
+
+
+def phi0(x):
+    return (sp.sqrt(2) - 1) * x + 1
+
+
+def phi0_df(x):
+    return (sp.sqrt(2) - 1)
+
+
+def phik(k, x):
+    return x**k * (1 - x)
+
+
+def phik_df(k, x):
+    return k * x**(k-1) * (1 - x) - x**k
+
+
+def u(ci, x_):
+    sigma = 0.0
+    for i in range(len(ci)):
+        sigma += ci[i] * (phik.subs({k: i + 1, x: x_}).evalf())
+    return (phi0.subs(x, x_).evalf() + sigma).evalf()
+
+
+def exact(x_):
+    return np.sqrt(x_ + 1)
+
+
+def equation(u):
+    return sp.diff(u, x, 2) + P * sp.diff(u, x, 1) + Q * u 
+
+
+# Количество C
+n = 2
+a = 0
+b = 1
+N = 50
+
+
+x = sp.Symbol('x')
+k = sp.Symbol('k')
+C = sp.IndexedBase('C')
+j = sp.symbols('j', cls=sp.Idx)
+u = sp.Symbol('u')
+
+F = F(sp.Symbol('x'))
+Q = Q(sp.Symbol('x'))
+P = P(sp.Symbol('x'))
+
+print("\nphi0    = ", phi0(x))
+#print(phi0(x).subs(x, 1).evalf())
+print("phi0_df = ", phi0_df(x))
+print("phik    =", phik(k, x))
+print("phik_df =", phik_df(k, x))
+
+# Задаем количество C_k
+C_k = [C[j] for j in range(0, n)]
+print("\nC_k = ", C_k)
+
+sigma_str = ""
+for i in range(0, n):
+    sigma_str += str(C_k[i] * phik(k, x).subs(k, i + 1))
+    if i != n - 1:
+        sigma_str += " + "
+sigma_phik = eval(sigma_str)
+print("sigma_phik = ", sigma_phik)  
+
+# Решение u в виде(4)
+ux = phi0(x) + sigma_phik
+print("\nu(x) = ", ux)
+
+# R(x, C1, ..., Cn)
+R = equation(ux)
+print("\nR(x, C1, ..., Cn) = ", R)
+
+
+systemCi = []
+for i in range(1, n + 1):
+    systemCi.append(sp.simplify(sp.integrate(phik(k, x).subs(k, i) * R, (x, a, b)).evalf()))
+
+print("\nsystemCi =", systemCi)
+
+
+
+#print(phi0.subs(x, 1).evalf())
+
+#xi = np.linspace(a, b, N + 1)
+
+#phi0 = phi0(sp.Symbol('x'))
+#phi0_df = phi0_df(sp.Symbol('x'))
+#phik = phik(sp.Symbol('k'), sp.Symbol('x'))
+#phik_df = phik_df(sp.Symbol('k'), sp.Symbol('x'))
+
+# Задаем количество C_k
+#C_k = [C[j] for j in range(0, n)]
+#print(C_k)
+
+#sigma_str = ""
+#for i in range(0, n):
+#    sigma_str += str(C_k[i] * phik_df.subs(k, i + 1))
+#    if i != n - 1:
+#        sigma_str += " + "
+#sigma1 = eval(sigma_str)
+#print("sigma1 = ", sigma1)
+
+#sigma_str = ""
+#for i in range(0, n):
+#    sigma_str += str(C_k[i] * phik.subs(k, i + 1))
+#    if i != n - 1:
+#        sigma_str += " + "
+#sigma2 = eval(sigma_str)
+#print("sigma2 = ", sigma2)
+
+# Считаем значения dФ / dC_i
+#systemCi = []
+#for i in range(1, n + 1):
+#    systemCi.append(sp.simplify(sp.integrate(2 * p * (phi0_df + sigma1) * phik_df.subs(k, i) -
+#                                             2 * q * (phi0 + sigma2) * phik.subs(k, i) + 2 * f * phik.subs(k, i),
+#                                             (x, a, b))).evalf())
+
+#for i in range(len(systemCi)):
+#    print(systemCi[i])
+
+#system = []
+#for i in range(len(systemCi)):
+#    system.append(systemCi[i])
+#print(system)
+
+# Решение получившейся системы
+#C_linsolve = sp.linsolve(system, [C_k[i] for i in range(0, n)])
+#C = list(C_linsolve)[0]
+#print("C_i values: ", C)
+
+# Численное решение
+#u_numeric = []
+#for i in range(N + 1):
+#    u_numeric.append(u(C, xi[i]))
+#print("u_numeric: ", u_numeric)
+
+# Точное решение
+#u_exact = []
+#for i in range(N + 1):
+    #    u_exact.append(exact(xi[i]))
+#print("u_exact: ", u_exact)
+
+# Визуализация
+#plt.figure(figsize=(15, 7))
+#sp = plt.subplot(121)
+
+#plt.plot(xi, u_numeric, 'k', label="Численное")
+#plt.plot(xi, u_exact, 'red', label="Точное")
+
+#plt.legend()
+#plt.grid(True)
+#plt.show()
